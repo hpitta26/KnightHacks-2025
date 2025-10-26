e69d46f2a5370e9f65a2763a00b531f0854422e5
Alex Arguelles <alexarguelles87923@gmail.com>
2025-10-26 00:54:06 -0400

    cors

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,12 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],  # React dev servers
+    allow_origins=[
+        "http://localhost:3000", 
+        "http://localhost:5173",  # React dev servers
+        "https://finterras.com",
+        "https://www.finterras.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
